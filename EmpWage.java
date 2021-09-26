e248d97855535a5d88ee0744d7aef0102d838759
prasadv1998 <prasadan1720@gmail.com>
2021-09-26 17:02:23 +0530

    Added: to print employee wage

--- EmpWage.java
+++ EmpWage.java
@@ -5,14 +5,16 @@ public class EmpWage {
 	public static final int Is_Part_Time = 2;
 	public static final int Emp_Rate_Per_Hour = 20;
 	public static final int Num_Of_Working_Days = 20;
+	public static final int Max_Hrs_In_Month = 100;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage");
 		int empHrs = 0;
-		int empWage = 0;
-		int totalEmpWage =0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays =0;
+		while (totalEmpHrs <= Max_Hrs_In_Month && totalWorkingDays < Num_Of_Working_Days )	{
+		totalWorkingDays++;
 		int empCheck = (int) Math.floor(Math.random() *10) %3;
-		for (int day = 0; day < Num_Of_Working_Days; day++) {
 		switch (empCheck) {
 			case Is_Full_Time:
 				empHrs = 8;
@@ -24,11 +26,11 @@ public class EmpWage {
 				break;
 			default:
 				empHrs = 0;
-		}	
-		empWage = empHrs * Emp_Rate_Per_Hour;
-		totalEmpWage += empWage;
-		System.out.println("Employee daily wage is:" +empWage);
+			}	
+			totalEmpHrs += empHrs;
+			System.out.println("Day#:" + totalWorkingDays + "Employee hour" +empHrs);
 		}
+		int totalEmpWage = totalEmpHrs * Emp_Rate_Per_Hour;
 		System.out.println("Total employee wage is:" + totalEmpWage);
 	}
 }	
