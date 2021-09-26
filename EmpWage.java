53371e3f8ba094bf235c5fced97fc74b2cec56bb
prasadv1998 <prasadan1720@gmail.com>
2021-09-26 16:26:09 +0530

    Added: to print employee wage per month

--- EmpWage.java
+++ EmpWage.java
@@ -4,25 +4,31 @@ public class EmpWage {
 	public static final int Is_Full_Time = 1;
 	public static final int Is_Part_Time = 2;
 	public static final int Emp_Rate_Per_Hour = 20;
+	public static final int Num_Of_Working_Days = 20;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage");
 		int empHrs = 0;
 		int empWage = 0;
+		int totalEmpWage =0;
 		int empCheck = (int) Math.floor(Math.random() *10) %3;
+		for (int day = 0; day < Num_Of_Working_Days; day++) {
 		switch (empCheck) {
 			case Is_Full_Time:
 				empHrs = 8;
 		System.out.println("Employee is Full Time");
-			break;
+				break;
 			case Is_Part_Time:
 				empHrs = 4;
 		System.out.println("Employee is Part Time");
-			break;
-		default:
+				break;
+			default:
 				empHrs = 0;
 		}	
 		empWage = empHrs * Emp_Rate_Per_Hour;
+		totalEmpWage += empWage;
 		System.out.println("Employee daily wage is:" +empWage);
+		}
+		System.out.println("Total employee wage is:" + totalEmpWage);
 	}
 }	
