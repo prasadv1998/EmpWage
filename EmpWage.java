df58852d7e54978a5ede87baba8f5195860029d9
prasadv1998 <prasadan1720@gmail.com>
2021-09-26 16:00:01 +0530

    Added: to print employee daily wage

--- EmpWage.java
+++ EmpWage.java
@@ -4,10 +4,20 @@ public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("welcome To Employee Wage");
 		int Is_Full_Time = 1;
-		double EmpCheck = Math.floor((Math.random() *10 ) %2);
-		if (EmpCheck == Is_Full_Time )
-			System.out.println("Employee Is Present");
-		else
-			System.out.println("Employee Is Obsent");		
+		int Emp_Wage_Per_Hour = 20;
+		
+		int empHrs = 0;
+		int empWage = 0;
+		
+		double empCheck = Math.floor(Math.random() * 10) % 	2; 
+			if (empCheck == Is_Full_Time ) {
+				System.out.println("Employee Is Present");
+				empHrs = 8;
+			}else {
+				System.out.println("Employee Is Abscent");
+				empHrs = 0;
+	}
+				empWage = empHrs * Emp_Wage_Per_Hour;
+				System.out.println("Daily Employee Wage Is:" + empWage);		
 	}
 }
