7ad6c96b1704e23c35bb9c8b33eb70fbb7cf8f2b
prasadv1998 <prasadan1720@gmail.com>
2021-09-26 15:54:18 +0530

    Added: to prin employee attendance

--- EmpWage.java
+++ EmpWage.java
@@ -3,5 +3,11 @@ package EmpWageProblem;
 public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("welcome To Employee Wage");
+		int Is_Full_Time = 1;
+		double EmpCheck = Math.floor((Math.random() *10 ) %2);
+		if (EmpCheck == Is_Full_Time )
+			System.out.println("Employee Is Present");
+		else
+			System.out.println("Employee Is Obsent");		
 	}
 }
