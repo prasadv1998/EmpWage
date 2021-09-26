d61562317b1da47a3015df1a7d1e210014b48ad6
prasadv1998 <prasadan1720@gmail.com>
2021-09-26 16:09:53 +0530

    Added: to print employee daily wage using switch case

--- EmpWage.java
+++ EmpWage.java
@@ -1,22 +1,28 @@
 package EmpWageProblem;
 
 public class EmpWage {
+	public static final int Is_Full_Time = 1;
+	public static final int Is_Part_Time = 2;
+	public static final int Emp_Rate_Per_Hour = 20;
+	
 	public static void main(String[] args) {
-		System.out.println("welcome To Employee Wage");
-		int Emp_Wage_Per_Hour = 20;
-		int Emp_Part_Time_Hour = 4;
-		int Emp_Full_Time_Hour = 8;
-		int Is_Full_Time = 1;
-		int Is_Part_Time = 2;
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if (empCheck == Is_Full_Time)	{
-			int EmpWage = Emp_Wage_Per_Hour * Emp_Full_Time_Hour;
-			System.out.println("Employee Wage For Full Time:"  +EmpWage);
-		}else if (empCheck == Is_Part_Time)	{
-			int EmpWage2 = Emp_Wage_Per_Hour * Emp_Part_Time_Hour;
-			System.out.println("Employee Wage For Part Time:"  +EmpWage2);
-		}else {
-			System.out.println("Employee Is Abscent");
-		}
-	}	
-}
+		System.out.println("Welcome to employee wage");
+		int empHrs = 0;
+		int empWage = 0;
+		int empCheck = (int) Math.floor(Math.random() *10) %3;
+		switch (empCheck) {
+			case Is_Full_Time:
+				empHrs = 8;
+		System.out.println("Employee is Full Time");
+			break;
+			case Is_Part_Time:
+				empHrs = 4;
+		System.out.println("Employee is Part Time");
+			break;
+		default:
+				empHrs = 0;
+		}	
+		empWage = empHrs * Emp_Rate_Per_Hour;
+		System.out.println("Employee daily wage is:" +empWage);
+	}
+}	
