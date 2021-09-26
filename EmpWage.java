fd489e8c0333c138c5abf8359ef2a7741d4d41dd
prasadv1998 <prasadan1720@gmail.com>
2021-09-26 16:06:10 +0530

    Added: to print emplyee part time

--- EmpWage.java
+++ EmpWage.java
@@ -3,21 +3,20 @@ package EmpWageProblem;
 public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("welcome To Employee Wage");
-		int Is_Full_Time = 1;
 		int Emp_Wage_Per_Hour = 20;
-		
-		int empHrs = 0;
-		int empWage = 0;
-		
-		double empCheck = Math.floor(Math.random() * 10) % 	2; 
-			if (empCheck == Is_Full_Time ) {
-				System.out.println("Employee Is Present");
-				empHrs = 8;
-			}else {
-				System.out.println("Employee Is Abscent");
-				empHrs = 0;
-	}
-				empWage = empHrs * Emp_Wage_Per_Hour;
-				System.out.println("Daily Employee Wage Is:" + empWage);		
-	}
+		int Emp_Part_Time_Hour = 4;
+		int Emp_Full_Time_Hour = 8;
+		int Is_Full_Time = 1;
+		int Is_Part_Time = 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if (empCheck == Is_Full_Time)	{
+			int EmpWage = Emp_Wage_Per_Hour * Emp_Full_Time_Hour;
+			System.out.println("Employee Wage For Full Time:"  +EmpWage);
+		}else if (empCheck == Is_Part_Time)	{
+			int EmpWage2 = Emp_Wage_Per_Hour * Emp_Part_Time_Hour;
+			System.out.println("Employee Wage For Part Time:"  +EmpWage2);
+		}else {
+			System.out.println("Employee Is Abscent");
+		}
+	}	
 }
